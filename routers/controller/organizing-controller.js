b87a7cf25c23cc159b54bb8150e0ff12f0d704dd
RashaSaud <91824052+RashaSaud@users.noreply.github.com>
2024-11-04 09:15:35 +0300

    organizing Api has been added

--- routers/controller/organizing-controller.js
+++ routers/controller/organizing-controller.js
@@ -13,4 +13,13 @@ const add_organizing = async (req,res)=>{
     }
 }
 
-module.exports = {add_organizing}
+const get_organizing = async (req, res) => {
+    try {
+      const getOrg = await organizing_committees.find({});
+      res.status(200).json(getOrg);
+    } catch (error) {
+      console.error('Error fetching organizing committees:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  };
+module.exports = {add_organizing,get_organizing}
--- routers/routes/organizing-route.js
+++ routers/routes/organizing-route.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const orgRoute = express.Router()
 
-const {add_organizing} = require('../controller/organizing-controller')
+const {add_organizing,get_organizing} = require('../controller/organizing-controller')
 orgRoute.post('/add-org',add_organizing)
-
+orgRoute.get('/get-org',get_organizing)
 module.exports = orgRoute
